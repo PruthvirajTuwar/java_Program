b6841e550c608ab3ec9eff328efdfe1622e66e57
Pruthviraj Tuwar <tuwarpruthviraj@gmail.com>
2025-10-17 12:11:53 +0530

    Add files via upload

---
+++ ClassWork/Day_02/program21.java
@@ -0,0 +1,22 @@
+
+class Marvellous
+{
+     public static void Display()
+    {
+    System.out.println("Jay Ganesh....");
+    System.out.println("Jay Ganesh....");
+    System.out.println("Jay Ganesh....");
+    System.out.println("Jay Ganesh....");
+
+    }
+}
+class program21
+{
+   
+
+    public  static void main (String A[])
+        {
+           Marvellous mobj = new Marvellous();
+           mobj.Display();
+        }
+}
